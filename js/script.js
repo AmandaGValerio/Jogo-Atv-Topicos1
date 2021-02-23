8b90b0cdaed3f5dae4f2884c727afe572e42bece
AmandaGValerio <valeriogamanda@gmail.com>
2021-02-22 21:04:27 -0300

    função para verificar vitoria
    versão 1 da função que verifica se o jogador ganhou o jogo (nas proporções menores)

--- js/script.js
+++ js/script.js
@@ -57,35 +57,150 @@ const processMove = () => {
     rodada++;
 }
 
-const verifyWinner = () => {
-    if (rodada % 4 > 0) {
-        if (matrizJogadas[0][0] === matrizJogadas[1][1] === matrizJogadas[2][2]
-            || matrizJogadas[0][0] === matrizJogadas[0][1] === matrizJogadas[0][2]
-            || matrizJogadas[0][0] === matrizJogadas[1][0] === matrizJogadas[2][0]) {
-            if(matrizJogadas[0][0] === 1)
-                player1++;
-            else
-                player2++;
-            partida++;
+function verificar() {
+    //pega a posição onde acabou 
+    var pos1, pos2;
+    var mod1 = pos1 % 3;
+    var mod2 = pos2 % 3;
+
+    //vasculha na linha
+    switch (mod1) {
+        case 0:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1 + mod1][pos2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1 + mod1 + 1][pos2] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+                    
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1 / 3][pos2 / 3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+        case 1:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1 + mod1][pos2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1 - mod1][pos2] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1 / 3][pos2 / 3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+        case 2:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1 - mod1][pos2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1 - mod1 - 1][pos2] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+                    
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1 / 3][pos2 / 3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+            
+    }
+
+    //vasculha na coluna
+    switch (mod2) {
+        case 0:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1][pos2 + mod2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1][pos2 + mod2 + 1] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1 / 3][pos2 / 3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+        case 1:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1][pos2 + mod2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1][pos2 - mod2] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1 / 3][pos2 / 3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+        case 2:
+            //se o valor for igual na do lado, verifica a próxima lateral
+            if (matrizJogadas[pos1][pos2 - mod2] == matrizJogadas[pos1][pos2]) {
+                //se sim, completou a sequencia
+                if (matrizJogadas[pos1][pos2 - mod2 - 1] == matrizJogadas[pos1][pos2]) {
+                    //marca o icone em tamanho grande
+
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1/3][pos2/3] = matrizJogadas[pos1][pos2];
+                    return;
+                }
+            }
+            break;
+    }    
+    //vasculha a diagonal, se a posição do centro já estiver preenchida
+    centro1 = Math.floor(pos1/3)*3 + 1;
+    centro2 = Math.floor(pos2/3)*3 + 1;
+    if(matrizJogadas[centro1][centro2] == matrizJogadas[pos1][pos2]){
+        if(matrizJogadas[centro1][centro2] == matrizJogadas[centro1--][centro2--] && matrizJogadas[centro1][centro2] == 
+            matrizJogadas[centro1++][centro2++]){
+                //marca o icone em tamanho grande
+                    
+                    //salva o valor na matriz binária
+                    matrizMaior[pos1/3][pos2/3] = matrizJogadas[pos1][pos2];
+                    return;
         }
-        if (matrizJogadas[0][1] === matrizJogadas[1][1] === matrizJogadas[2][1]
-            || matrizJogadas[1][0] === matrizJogadas[1][1] === matrizJogadas[1][2]) {
-                if(matrizJogadas[1][1] === 1)
-                player1++;
-            else
-                player2++;
-            partida++;
-        }
-        if (matrizJogadas[0][2] === matrizJogadas[1][2] === matrizJogadas[2][2]
-            || matrizJogadas[2][0] === matrizJogadas[2][1] === matrizJogadas[2][2]) {
-                if(matrizJogadas[2][2] === 1)
-                player1++;
-            else
-                player2++;
-            partida++;
+        else if(matrizJogadas[centro1][centro2] == matrizJogadas[centro1--][centro2++] && matrizJogadas[centro1][centro2] == 
+            matrizJogadas[centro1++][centro2--]){
+                //marca o icone em tamanho grande
+                
+                //salva o valor na matriz binária
+                matrizMaior[pos1/3][pos2/3] = matrizJogadas[pos1][pos2];
+                return;
         }
     }
-    if(partida > 2){
-        return
-    }
+
+
+        /*     if (rodada % 4 > 0) {
+                if (matrizJogadas[0][0] === matrizJogadas[1][1] === matrizJogadas[2][2]
+                    || matrizJogadas[0][0] === matrizJogadas[0][1] === matrizJogadas[0][2]
+                    || matrizJogadas[0][0] === matrizJogadas[1][0] === matrizJogadas[2][0]) {
+                    if(matrizJogadas[0][0] === 1)
+                        player1++;
+                    else
+                        player2++;
+                    partida++;
+                }
+                if (matrizJogadas[0][1] === matrizJogadas[1][1] === matrizJogadas[2][1]
+                    || matrizJogadas[1][0] === matrizJogadas[1][1] === matrizJogadas[1][2]) {
+                        if(matrizJogadas[1][1] === 1)
+                        player1++;
+                    else
+                        player2++;
+                    partida++;
+                }
+                if (matrizJogadas[0][2] === matrizJogadas[1][2] === matrizJogadas[2][2]
+                    || matrizJogadas[2][0] === matrizJogadas[2][1] === matrizJogadas[2][2]) {
+                        if(matrizJogadas[2][2] === 1)
+                        player1++;
+                    else
+                        player2++;
+                    partida++;
+                }
+            }
+            if(partida > 2){
+                return
+            } */ 
 }
+
